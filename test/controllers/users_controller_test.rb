826f8cf0c543d340c761700ebc8d8f876250e988
chouchou <chaudiemhoang2@gmail.com>
2024-09-23 14:45:44 +0700

    fix rubocop

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-
   test 'setup' do
     @user = users(:michael)
     @other_user = users(:archer)
@@ -12,7 +11,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should redirect index when not logged in" do
+  test 'should redirect index when not logged in' do
     get users_path
     assert_redirected_to login_url
   end
@@ -24,32 +23,32 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect update when not logged in' do
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    patch user_path(@user), params: {user: {name: @user.name, email: @user.email}}
     assert_not flash.empty?
     assert_redirected_to login_url
   end
 
-  test "should not allow the admin attribute to be edited via the web" do
+  test 'should not allow the admin attribute to be edited via the web' do
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
-                                  user: { password: "password",
-                                  password_confirmation: "password",
-                                  admin: true } }
+                                  user: {password: 'password',
+                                  password_confirmation: 'password',
+                                  admin: true}}
     assert_not @other_user.reload.admin?
   end
 
   test 'should redirect edit when logged in as wrong user' do
     log_in_as(users(:michael))
     get edit_user_path(@user)
-    assert flash.empty?
+    assert_predicate flash, :empty?
     assert_redirected_to root_url
   end
 
   test 'should redirect update when logged in as wrong user' do
     log_in_as(users(:michael))
-    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
-    assert flash.empty?
+    patch user_path(@user), params: {user: {name: @user.name, email: @user.email}}
+    assert_predicate flash, :empty?
     assert_redirected_to root_url
   end
 
