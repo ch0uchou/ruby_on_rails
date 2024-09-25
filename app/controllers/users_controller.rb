f48fd146984220bce22d28af41f8bd21269f1160
chouchou <chaudiemhoang2@gmail.com>
2024-09-25 12:03:51 +0700

    resovle cmts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,19 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+  before_action :load_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page])
   end
+
   def show
-    @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,11 +25,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
   def edit
-    @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -33,6 +37,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = 'User deleted'
@@ -48,21 +53,25 @@ class UsersController < ApplicationController
   # Before filters
   # Confirms a logged-in user
   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 
   # Confirms the correct user
   def correct_user
-    @user = User.find(params[:id])
+    load_user
     redirect_to(root_url) unless current_user?(@user)
   end
 
   # Confirms an admin user
   def admin_user
     redirect_to(root_url) unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find(params[:id])
   end
 end
