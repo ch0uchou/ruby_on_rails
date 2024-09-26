20aeb09b14ef9e0b97001bdd23a5da9cd963f6c5
chouchou <chaudiemhoang2@gmail.com>
2024-09-26 09:45:48 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
   get 'sessions/new'
-  resources :microposts
-  resources :users
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
