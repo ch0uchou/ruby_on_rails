d1ecc3f40ade4af9eec4a546017db28d6fb488a1
chouchou <chaudiemhoang2@gmail.com>
2024-09-26 15:21:55 +0700

    fix rubocop

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     return unless logged_in?
-    
+
     @micropost = current_user.microposts.build
     @feed_items = current_user.feed.includes(:user).paginate(page: params[:page])
   end
